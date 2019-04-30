eed2746691fe38f93eaf6207f26aa21ed09db39e
wptjd93 <42082678+wptjd93@users.noreply.github.com>
2019-05-01 00:17:00 +0900

    Update main.py

--- main.py
+++ main.py
@@ -14,11 +14,11 @@ while True:
     cv2.imshow('window2',cv2.cvtColor(original_image, cv2.COLOR_BGR2RGB))
     if dis<30:
         stop()
-    elif m1 < 0 and m2 < 0:     # / /
+    elif m1 < 0 and m2 < 0:     
         right()
-    elif m1 > 0  and m2 > 0:  # \ \
+    elif m1 > 0  and m2 > 0: 
         left()
-    else:                     # / \
+    else:               
         straight()
     if cv2.waitKey(25) & 0xFF == ord('q'):
         break
